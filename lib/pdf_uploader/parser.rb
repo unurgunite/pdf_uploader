881095f15529b5fc3297b3d3e3480d15117183f1
unurgunite <senpaiguru1488@gmail.com>
2023-01-01 18:53:24 +0300

    Updated parser

--- lib/pdf_uploader/parser.rb
+++ lib/pdf_uploader/parser.rb
@@ -4,13 +4,13 @@ module PdfUploader
   module Parser
     class << self
       def parse!(url)
-        url = PdfUploader::URL.new(url).update_url!
         site = HTTParty.get(url)
         document = Nokogiri::HTML(site)
-        document.search('a').each_with_object([]) do |el, arr|
-          next if (href = el['href']) == '../'
+        document.search('a').each_with_object(Array.new(2) { |i| [] if i.zero? }).with_index(1) do |(el, arr), i|
+          next if (href = el['href']) == '../' || href.end_with?('/')
 
-          arr << "#{url}#{href}"
+          arr[0] << "#{url}#{href}"
+          arr[1] = i
         end
       end
     end
--- lib/pdf_uploader/url.rb
+++ lib/pdf_uploader/url.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 module PdfUploader
   class URL < DelegateClass(String)
     def update_url!
-      end_with?('/') ? self : "#{self}/"
+      end_with?('/') ? self : PdfUploader::URL.new("#{self}/")
+    end
+
+    def url?
+      uri = URI.parse(self)
+      pp self
+      HTTParty.get(self).code == 200 if uri.is_a?(URI::HTTP) && !uri.host.nil?
+    rescue URI::InvalidURIError
+      false
     end
   end
 end
