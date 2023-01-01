dbe9baee6c382664aaae91468e3bcaa44469e524
unurgunite <senpaiguru1488@gmail.com>
2023-01-01 19:31:24 +0300

    Added Apache links detection

--- lib/pdf_uploader/parser.rb
+++ lib/pdf_uploader/parser.rb
@@ -7,11 +7,17 @@ module PdfUploader
         site = HTTParty.get(url)
         document = Nokogiri::HTML(site)
         document.search('a').each_with_object(Array.new(2) { |i| [] if i.zero? }).with_index(1) do |(el, arr), i|
-          next if (href = el['href']) == '../' || href.end_with?('/')
+          next if (href = el['href']) == '../' || href.end_with?('/') || apache?(href)
 
           arr[0] << "#{url}#{href}"
           arr[1] = i
         end
+      end
+
+      private
+
+      def apache?(href)
+        href.match?(/C=\w+;O=\w+/)
       end
     end
   end
